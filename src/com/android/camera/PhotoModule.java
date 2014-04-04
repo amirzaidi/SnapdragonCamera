b9680030697927c15aa68034e96692782db6034d
Kerong Sui <ksui@codeaurora.org>
2014-04-04 11:13:50 +0800

    Camera2: fix the picture format not update problem
    When we set the ZSL on, the picture format should change to
    Jpeg immediately. So we override the setting menu.
    
    Change-Id: Ie24acf61667a7faa7603d8c3876bcba0ec0da5c2
    CRs-fixed: 621806

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2476,6 +2476,7 @@ public class PhotoModule
             Editor editor = mPreferences.edit();
             editor.putString(CameraSettings.KEY_PICTURE_FORMAT, mActivity.getString(R.string.pref_camera_picture_format_value_jpeg));
             editor.apply();
+            mUI.overrideSettings(CameraSettings.KEY_PICTURE_FORMAT, mActivity.getString(R.string.pref_camera_picture_format_entry_jpeg));
 
             //Try to set CAF for ZSL
             if(CameraUtil.isSupported(Parameters.FOCUS_MODE_CONTINUOUS_PICTURE,
