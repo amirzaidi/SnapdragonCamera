752bc16ccf95572d843cbfd8a2b30ed747b246f3
Suman Mukherjee <sumam@codeaurora.org>
2014-04-28 10:36:56 +0530

    Camera: Don't set brightness progressbar visible if not set
    Through skintonedisable brightnessprogressbar is getting enabled,
    though that hasn't got triggered.
    
    Fix Made: Use the unused brightnessvisible flag to identify whether
    brightness had been triggered or not. If it hasn't got triggered,
    then don't enable the progressbar in skintonedisable method.
    
    Change-Id: I72960fb206fb758dd4e330341f5b4f5267d97ac5
    CRs-Fixed: 653129

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -105,7 +105,7 @@ public class PhotoModule
     private static final String TAG = "CAM_PhotoModule";
 
    //QCom data members
-    public static boolean mBrightnessVisible = true;
+    public static boolean mBrightnessVisible = false;
     private static final int MAX_SHARPNESS_LEVEL = 6;
     private boolean mRestartPreview = false;
     private int mSnapshotMode;
@@ -2158,6 +2158,7 @@ public class PhotoModule
                 }
                 brightnessProgressBar.setProgress(mbrightness);
                 brightnessProgressBar.setVisibility(View.VISIBLE);
+                mBrightnessVisible = true;
             }
             break;
            case KeyEvent.KEYCODE_DPAD_RIGHT:
@@ -2175,6 +2176,7 @@ public class PhotoModule
                 }
                 brightnessProgressBar.setProgress(mbrightness);
                 brightnessProgressBar.setVisibility(View.VISIBLE);
+                mBrightnessVisible = true;
             }
             break;
             case KeyEvent.KEYCODE_DPAD_CENTER:
@@ -3327,7 +3329,7 @@ public class PhotoModule
         editor.putString(CameraSettings.KEY_SKIN_TONE_ENHANCEMENT_FACTOR,
             Integer.toString(mskinToneValue - MIN_SCE_FACTOR));
         editor.apply();
-        if(brightnessProgressBar != null)
+        if ((brightnessProgressBar != null) && mBrightnessVisible)
              brightnessProgressBar.setVisibility(View.VISIBLE);
 }
 
