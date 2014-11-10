0462353d6c976127828edb859d09b7799377795b
kaiyiz <kaiyiz@codeaurora.org>
2014-11-10 14:57:54 +0800

    SnapdragonCamera: Fix normal app could not access SDCard
    Only system application which was signed with platform certification
    can access the external SD card.
    
    Add 'LOCAL_CERTIFICATE := platform' in the 'Android.mk' of the
    application.
    
    CRs-Fixed: 752264
    
    Change-Id: Ia84e7819b93dc429aac0bee601bd299c967d5457

--- Android.mk
+++ Android.mk
@@ -14,6 +14,7 @@ LOCAL_SRC_FILES += $(call all-java-files-under, src_pd_gcam)
 
 LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
 
+LOCAL_CERTIFICATE := platform
 
 include $(LOCAL_PATH)/version.mk
 LOCAL_AAPT_FLAGS := \
