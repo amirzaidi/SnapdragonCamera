1e912d03bfc52ebae890821ee8f5a385ca2d56ec
Emilian Peev <epeevs@codeaurora.org>
2014-12-16 11:04:30 +0200

    Snapdragon Camera: Unlock AE&AWB before snapshot
     Currently the application contains logic which
     will lock AE&AWB before snapshot if AF didn't
     get triggered. Unlocking happens during image
     capture though and will get ignored because
     the camera state is already set to
     'SNAPSHOT_IN_PROGRESS". This sequence of events
     can have side effects w.r.t. flash. To avoid
     this AE&AWB should always be unlocked before
     snapshot.
    
    Change-Id: I7b1d53fdd87f7e9c1a1bb3530e4c5a87a7b05605

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1514,12 +1514,13 @@ public class PhotoModule
             mParameters.remove(CameraSettings.KEY_QC_LEGACY_BURST);
         }
 
-        // Unlock AE&AWB during longshot
-        if (LONGSHOT == mCameraState) {
-            mFocusManager.setAeAwbLock(false);
-            setAutoExposureLockIfSupported();
-            setAutoWhiteBalanceLockIfSupported();
-        }
+        // Unlock AE&AWB, if they continue
+        // to be locked during snapshot, then
+        // side effects could be triggered w.r.t.
+        // flash.
+        mFocusManager.setAeAwbLock(false);
+        setAutoExposureLockIfSupported();
+        setAutoWhiteBalanceLockIfSupported();
 
         mCameraDevice.setParameters(mParameters);
         mParameters = mCameraDevice.getParameters();
