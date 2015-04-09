ed7f84007665f4f0bcd7dcf709cc39686336531e
likaid <likaid@codeaurora.org>
2015-04-09 11:54:35 +0800

    SnapdragonCamera: Customize camera video quality
    SnapdragonCamera's default video quality value was set to highest
    supported video resolution whitch was "720P"
    
    Add the default video quality value string "pref_video_quality_default"
    and overlay it to "VGA"
    
    CRs-Fixed: 815638
    Change-Id: I5013880a42028e0663a3d22ffa3cddf87990146b

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -780,10 +780,16 @@ public class VideoModule implements CameraModule,
                         null);
         if (videoQuality == null) {
             mParameters = mCameraDevice.getParameters();
+            String defaultQuality = mActivity.getResources().getString(
+                    R.string.pref_video_quality_default);
+            if (!defaultQuality.equals("")){
+                videoQuality = defaultQuality;
+            } else {
             // check for highest quality supported
             videoQuality = CameraSettings.getSupportedHighestVideoQuality(
                     mCameraId, mParameters);
-            mPreferences.edit().putString(CameraSettings.KEY_VIDEO_QUALITY, videoQuality);
+            }
+            mPreferences.edit().putString(CameraSettings.KEY_VIDEO_QUALITY, videoQuality).apply();
         }
         int quality = CameraSettings.VIDEO_QUALITY_TABLE.get(videoQuality);
 
