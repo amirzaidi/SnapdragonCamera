ebf9dcc648cb5faade3a59d155f696e7927923d0
Santhosh Behara <santhoshbehara@codeaurora.org>
2016-04-27 18:15:24 -0700

    SnapdragonCamera: Use mediarecorder resume instead of start
    For camcorder to resume after pause use mediarecorder's  resume()
    API instead of start() API
    
    Change-Id: Ieb6e2c681ed5403b26bfc522c7dfef41104e3653

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1902,7 +1902,7 @@ public class VideoModule implements CameraModule,
         mMediaRecorderPausing = false;
         mRecordingStartTime = SystemClock.uptimeMillis();
         updateRecordingTime();
-        mMediaRecorder.start();
+        mMediaRecorder.resume();
     }
 
     private boolean stopVideoRecording() {
