382e2cb9c930a129aca9e83315660e8172786fb4
Jack Yoo <jyoo@codeaurora.org>
2016-07-13 12:56:36 -0700

    SnapdragonCamera: Adding PRIVILEGED true
    Promoting privilege for direct SD card path access.
    
    Change-Id: I10f28606a646a85c0cb23e4cf8e617eafdcaa2b4
    CRs-Fixed: 1032564

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ LOCAL_AAPT_FLAGS := \
         --version-code $(version_code_package) \
 
 LOCAL_PACKAGE_NAME := SnapdragonCamera
+LOCAL_PRIVILEGED_MODULE := true
 
 #LOCAL_SDK_VERSION := current
 LOCAL_RENDERSCRIPT_TARGET_API := 23
