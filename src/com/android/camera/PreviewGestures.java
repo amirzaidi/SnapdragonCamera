0321ab092671d680373bff35673fa6a069d7e57e
Byunghun Jeon <bjeon@codeaurora.org>
2016-07-28 11:07:46 -0700

    SnapdragonCamera: Remove swipe right to open menu
    Remove swipe right to open menu.
    
    CRs-Fixed: 1028467
    Change-Id: Ib90a3002e2e6815b5d6f7aea8fdf73aafbe527ee

--- src/com/android/camera/PreviewGestures.java
+++ src/com/android/camera/PreviewGestures.java
@@ -87,29 +87,8 @@ public class PreviewGestures
                 return false;
             }
             if (mZoomOnly || mMode == MODE_ZOOM) return false;
-            int deltaX = (int) (e1.getX() - e2.getX());
-            int deltaY = (int) (e1.getY() - e2.getY());
 
-            int orientation = 0;
-            if (mPhotoMenu != null)
-                orientation = mPhotoMenu.getOrientation();
-            else if (mVideoMenu != null)
-                orientation = mVideoMenu.getOrientation();
-            else if (mCaptureUI != null)
-                orientation = mCaptureUI.getOrientation();
-
-            if (isLeftSwipe(orientation, deltaX, deltaY)) {
-                waitUntilNextDown = true;
-                if (mPhotoMenu != null && !mPhotoMenu.isMenuBeingShown())
-                    mPhotoMenu.openFirstLevel();
-                else if (mVideoMenu != null && !mVideoMenu.isMenuBeingShown())
-                    mVideoMenu.openFirstLevel();
-                else if (mCaptureUI != null && !mCaptureUI.isMenuBeingShown())
-                    mCaptureUI.showSettingMenu();
-                return true;
-            } else {
-                return onSingleTapUp(e2);
-            }
+            return onSingleTapUp(e2);
         }
 
         private boolean isLeftSwipe(int orientation, int deltaX, int deltaY) {
