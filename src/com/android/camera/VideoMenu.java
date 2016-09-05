68359ee7df739df7aeda2d542dbbc884f613147b
mingwax <mingwax@codeaurora.org>
2016-09-05 15:10:43 +0800

    SnapdragonCamera: Fix OutOfBoundsException in monkey test
    In monkey test, set defaultValue operation in xml file, parsing
    XML in framework code, pref.getValue() return null when
    defaultValue not ready. When findIndexOfValue() failed to get
    the index of the value, it will return -1, but -1 can`t be the
    index of the array, it will cause exception. So if can`t find
    the index of the value, just go back and don`t execute the
    following code.
    
    Change-Id: I2b5595d7d558e3ab3863287e3d1424df141c5d59
    CRs-Fixed: 1063228

--- src/com/android/camera/VideoMenu.java
+++ src/com/android/camera/VideoMenu.java
@@ -472,6 +472,9 @@ public class VideoMenu extends MenuController
         int resid = -1;
         int index = pref.findIndexOfValue(pref.getValue());
         if (!pref.getUseSingleIcon() && iconIds != null) {
+            if (index == -1) {
+                return;
+            }
             // Each entry has a corresponding icon.
             resid = iconIds[index];
         } else {
