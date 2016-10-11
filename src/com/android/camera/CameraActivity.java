6fc6fdf1f4109ca0d065517b1a2ffd46d255b946
weijiew <weijiew@codeaurora.org>
2016-10-11 15:58:30 +0800

    SnapdragonCamera: Camera force close
    When finish once capture picture, camera need get available space
    of SDCard/Internal storage.It is a I/O operation and may block a
    few millisecond. Currently camera get available space by AsyncTask.
    When available space was smaller than 60M, storage will switch to
    sdcard and update UI which cased crash.
    
    Change-Id: Ia7790121dfc5321da4e621d1ff4e31f49cfc5a77
    CRs-Fixed: 1072961

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -161,6 +161,8 @@ public class CameraActivity extends Activity
     private static final int HIDE_ACTION_BAR = 1;
     private static final long SHOW_ACTION_BAR_TIMEOUT_MS = 3000;
 
+    private static final int SWITCH_SAVE_PATH = 2;
+
     /** Permission request code */
     private static final int PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
 
@@ -364,6 +366,8 @@ public class CameraActivity extends Activity
             if (msg.what == HIDE_ACTION_BAR) {
                 removeMessages(HIDE_ACTION_BAR);
                 CameraActivity.this.setSystemBarsVisibility(false);
+            }else if ( msg.what == SWITCH_SAVE_PATH ) {
+                mCurrentModule.onSwitchSavePath();
             }
         }
     }
@@ -1887,7 +1891,7 @@ public class CameraActivity extends Activity
             mStorageSpaceBytes = Storage.getAvailableSpace();
             if (Storage.switchSavePath()) {
                 mStorageSpaceBytes = Storage.getAvailableSpace();
-                mCurrentModule.onSwitchSavePath();
+                mMainHandler.sendEmptyMessage(SWITCH_SAVE_PATH);
             }
             return mStorageSpaceBytes;
         }
