9a351414d4e634f09313ca27c286d919b1b43636
mingwax <mingwax@codeaurora.org>
2016-11-30 14:42:27 +0800

    SnapdragonCamera: Keep screen on when recording the video
    Add the actions to keep the screen on or off.
    (1) We always keep the screen on when click video button for recording
        video.
    (2) We keep the screen on for 2 mins when click video button for stop
        recording video.
    (3) We clear the flag(FLAG_KEEP_SCREEN_ON) when exit
        the camera.
    
    CRs-Fixed: 1095512
    change-Id: I283c29882f0654315536afbafc5072491343a38f

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -72,6 +72,7 @@ import android.view.OrientationEventListener;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.android.camera.exif.ExifInterface;
@@ -167,6 +168,8 @@ public class CaptureModule implements CameraModule, PhotoController,
     private static final int LONGSHOT_CANCEL_THRESHOLD = 40 * 1024 * 1024;
 
     private static final int NORMAL_SESSION_MAX_FPS = 60;
+
+    private static final int SCREEN_DELAY = 2 * 60 * 1000;
 
     MeteringRectangle[][] mAFRegions = new MeteringRectangle[MAX_NUM_CAM][];
     MeteringRectangle[][] mAERegions = new MeteringRectangle[MAX_NUM_CAM][];
@@ -289,6 +292,7 @@ public class CaptureModule implements CameraModule, PhotoController,
     private int mTimeBetweenTimeLapseFrameCaptureMs = 0;
     private boolean mCaptureTimeLapse = false;
     private CamcorderProfile mProfile;
+    private static final int CLEAR_SCREEN_DELAY = 4;
     private static final int UPDATE_RECORD_TIME = 5;
     private ContentValues mCurrentVideoValues;
     private String mVideoFilename;
@@ -1893,6 +1897,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         if (selfieThread != null) {
             selfieThread.interrupt();
         }
+        resetScreenOn();
         mUI.stopSelfieFlash();
     }
 
@@ -2675,6 +2680,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                         mUI.enableShutter(false);
                         mUI.showRecordingUI(true, true);
                         updateRecordingTime();
+                        keepScreenOn();
                     }
 
                     @Override
@@ -2706,6 +2712,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                         mRecordingStartTime = SystemClock.uptimeMillis();
                         mUI.showRecordingUI(true, false);
                         updateRecordingTime();
+                        keepScreenOn();
                     }
 
                     @Override
@@ -2911,7 +2918,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         if (shouldAddToMediaStoreNow) {
             saveVideo();
         }
-
+        keepScreenOnAwhile();
         // release media recorder
         releaseMediaRecorder();
         releaseAudioFocus();
@@ -3928,6 +3935,11 @@ public class CaptureModule implements CameraModule, PhotoController,
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
+                case CLEAR_SCREEN_DELAY: {
+                    mActivity.getWindow().clearFlags(
+                            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    break;
+                }
                 case UPDATE_RECORD_TIME: {
                     updateRecordingTime();
                     break;
@@ -4025,5 +4037,21 @@ public class CaptureModule implements CameraModule, PhotoController,
         }
         mToast.setText(tips);
         mToast.show();
+    }
+
+    private void resetScreenOn() {
+        mHandler.removeMessages(CLEAR_SCREEN_DELAY);
+        mActivity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    private void keepScreenOnAwhile() {
+        mHandler.removeMessages(CLEAR_SCREEN_DELAY);
+        mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        mHandler.sendEmptyMessageDelayed(CLEAR_SCREEN_DELAY, SCREEN_DELAY);
+    }
+
+    private void keepScreenOn() {
+        mHandler.removeMessages(CLEAR_SCREEN_DELAY);
+        mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
