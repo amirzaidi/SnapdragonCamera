945c0c226235d8c7bf027a42bb9db3a6bdf4e0c0
junjiez <junjiez@codeaurora.org>
2017-01-10 10:42:35 +0800

    SnapdragonCamera: Fix compile error on Android M
    Declare jpegData as a final variable to avoid
    complie error on Android M.
    
    Change-Id: Ic5048525657f5f0cf54c0154f6043b5166adbb92
    CRs-Fixed: 1095457

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -1216,7 +1216,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         mJpegImageData = data;
     }
 
-    public void showCapturedReview(byte[] jpegData, int orientation, boolean mirror) {
+    public void showCapturedReview(final byte[] jpegData, int orientation, boolean mirror) {
         mActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
