7b7247768e2c4c891329d60e965bbbc4853700d0
junjiez <junjiez@codeaurora.org>
2017-01-19 16:54:02 +0800

    napdragonCamera: Fix crash when swith to fisheye
    Add judgement to avoid crash when preference of
    CameraSettings.KEY_VIDEO_HIGH_FRAME_RATE is null.
    
    Change-Id: Ifb8ffd585b6bc802d2252d63709da5d178bbd55d
    CRs-Fixed: 1111662

--- src/com/android/camera/VideoMenu.java
+++ src/com/android/camera/VideoMenu.java
@@ -776,21 +776,25 @@ public class VideoMenu extends MenuController
                 .getInt("persist.camcorder.eis.maxfps", 30);
         ListPreference hfrPref = mPreferenceGroup
                 .findPreference(CameraSettings.KEY_VIDEO_HIGH_FRAME_RATE);
-        if (hfrPref != null) {
-            String highFrameRate = hfrPref.getValue();
-            boolean isHFR = "hfr".equals(highFrameRate.substring(0,3));
-            boolean isHSR = "hsr".equals(highFrameRate.substring(0,3));
-            int rate = 0;
-            if ( isHFR || isHSR ) {
-                String hfrRate = highFrameRate.substring(3);
-                rate = Integer.parseInt(hfrRate);
-            }
+        String highFrameRate;
+        if (hfrPref == null) {
+            //If hfrPref is null, use whitespace instead.
+            highFrameRate = "     ";
+        } else {
+            highFrameRate = hfrPref.getValue();
+        }
+        boolean isHFR = "hfr".equals(highFrameRate.substring(0,3));
+        boolean isHSR = "hsr".equals(highFrameRate.substring(0,3));
+        int rate = 0;
+        if ( isHFR || isHSR ) {
+            String hfrRate = highFrameRate.substring(3);
+            rate = Integer.parseInt(hfrRate);
+        }
 
-            if ((disMode.equals("enable") && rate > PERSIST_EIS_MAX_FPS)
-                    || !videoHDR.equals("off")
-                    || timeLapseInterval != 0) {
-                mListMenu.setPreferenceEnabled(CameraSettings.KEY_VIDEO_HIGH_FRAME_RATE, false);
-            }
+        if ((disMode.equals("enable") && rate > PERSIST_EIS_MAX_FPS)
+                || !videoHDR.equals("off")
+                || timeLapseInterval != 0) {
+            mListMenu.setPreferenceEnabled(CameraSettings.KEY_VIDEO_HIGH_FRAME_RATE, false);
         }
 
     }
