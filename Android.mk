34391dbc7835fe42fe303eb4ed432b830a8653fd
Amir Zaidi <azaidi@live.nl>
2017-06-20 16:20:57 +0200

    Change name and icon to Camera and automatically include libraries
    Change-Id: Ie23498bee8c03099ed939a0dfb4ceba9349fbcfd

--- Android.mk
+++ Android.mk
@@ -34,11 +34,11 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 # If this is an unbundled build (to install separately) then include
 # the libraries in the APK, otherwise just put them in /system/lib and
 # leave them out of the APK
-ifneq (,$(TARGET_BUILD_APPS))
+#ifneq (,$(TARGET_BUILD_APPS))
   LOCAL_JNI_SHARED_LIBRARIES := libjni_snapcammosaic libjni_snapcamtinyplanet libjni_imageutil
-else
-  LOCAL_REQUIRED_MODULES := libjni_snapcammosaic libjni_snapcamtinyplanet libjni_imageutil
-endif
+#else
+#  LOCAL_REQUIRED_MODULES := libjni_snapcammosaic libjni_snapcamtinyplanet libjni_imageutil
+#endif
 
 include $(BUILD_PACKAGE)
 
