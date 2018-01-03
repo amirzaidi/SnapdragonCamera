a6011433c0a32ce2546b5d149a1b6bcd9327d269
zhuw <zhuw@codeaurora.org>
2018-01-03 15:34:00 +0800

    Fix snapshot failed with logical camera
    if ZSl is enabled , use takeZSLpicture and return
    
    Change-Id: I501b40218daef5a5f2847426df00ab18743ee63f

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -1353,6 +1353,9 @@ public class CaptureModule implements CameraModule, PhotoController,
                         lockFocus(MONO_ID);
                         break;
                     case SWITCH_MODE:
+                        if (takeZSLPicture(SWITCH_ID)) {
+                            return;
+                        }
                         lockFocus(SWITCH_ID);
                         break;
                 }
